ab196fe8cc063f24e0f9f05df135e7725bdbe5b5
Vinay D'Souza <vinucondev@gmail.com>
2013-07-09 18:59:52 +0000

    Updated web.js to take index stuff

--- web.js
+++ web.js
@@ -2,8 +2,15 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var buffer = newBuffer(fs.readFileSync('index.html','utf8'), "utf-8");
+
+console.log(fs.readFileSync('index.html','utf8'));
+
+console.log(buffer.toString('utf-8'));
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  //response.send('Hello World 2!');
+    response.send(buffer.toString('utf-8');
 });
 
 var port = process.env.PORT || 5000;
